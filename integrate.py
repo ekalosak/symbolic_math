26dd524deb1a0528b36c0e84ebe0b38dc7c49724
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-10-07 09:36:24 -0400

    successfully implemented symbolic polynomial integration

--- integrate.py
+++ integrate.py
@@ -14,12 +14,17 @@ def rep_poly(p):
 def int_poly(p):
     ''' integrate vector representation of the polynomial'''
     fp = [float(a) for a in p]
-    ip = [0] + [x/i for x, i in zip(
-
+    fr = [float(a) for a in range(1, len(p) + 1)]
+    ip = [0] + [x/i for x, i in zip(fp, fr)]
+    return ip
 
 def tests():
     p1 = [1, 2, 4, 6]
+    ip1 = int_poly(p1)
     print 'Integrating polynomial: {}'.format(rep_poly(p1))
-    assert int_poly(p1) == [0, 1, 
+    print 'Integrated to: {}'.format(rep_poly(ip1))
+    assert ip1 == [0, 1, 1, 4.0/3, 6.0/4]
+    print "Passed tests!!"
+
 if __name__ == '__main__':
     tests()
